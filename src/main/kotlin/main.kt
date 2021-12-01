094ac5917809a0e3b88bbaf3f837214820435f54
piotr.sekula <piotr92345@o2.pl>
2021-12-01 19:40:22 +0100

    Solve 1st task

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    val mutableList = mutableListOf<Int>()
+    File("/home/piotrek/Workspace/AoC2021/src/main/resources/1_input.txt")
+        .forEachLine { mutableList.add(it.toInt()) }
+
+    var increaseCount = 0
+    for (index in mutableList.indices) {
+        println("Counting for: $index")
+        if (index > 0) {
+            if(mutableList[index] - mutableList[index - 1] > 0) {
+                increaseCount += 1
+            }
+        }
+    }
+
+    println("No of increases=  $increaseCount")
 }
