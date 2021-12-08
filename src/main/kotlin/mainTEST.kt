c41f8d4519597a6831115b04d182c46860d7b5ee
piotrsekula <piotr.sekula@tesco.com>
2021-12-08 14:06:52 +0100

    Start implementation of 11th task

---
+++ src/main/kotlin/mainTEST.kt
@@ -0,0 +1,54 @@
+fun main(args: Array<String>) {
+    println("Hello Task TEST!")
+
+    val genA = Fish(79)
+
+    val genB = genA.noOfChildrenList()
+    println("No of children: ${genB.size}")
+//    genB.forEach { x -> println(x) }
+
+    val genC = genB.flatMap { gb -> gb.noOfChildrenList() }
+    println("No of children: ${genC.size}")
+//    genC.forEach { x -> println(x) }
+
+    val genD = genC.flatMap { gc -> gc.noOfChildrenList() }
+    println("No of children: ${genD.size}")
+//    genD .forEach { x -> println(x) }
+
+    val genE = genD.flatMap { gd -> gd.noOfChildrenList() }
+    println("No of children: ${genE.size}")
+//    genE .forEach { x -> println(x) }
+
+    val genF = genE.flatMap { ge -> ge.noOfChildrenList() }
+    println("No of children: ${genF.size}")
+
+    val genG = genF.flatMap { gf -> gf.noOfChildrenList() }
+    println("No of children: ${genG.size}")
+
+    val genH = genG.flatMap { gg -> gg.noOfChildrenList() }
+    println("No of children: ${genH.size}")
+
+    val genI = genH.flatMap { gh -> gh.noOfChildrenList() }
+    println("No of children: ${genI.size}")
+
+    val genJ = genI.flatMap { gi -> gi.noOfChildrenList() }
+    println("No of children: ${genJ.size}")
+}
+
+class Fish(private val daysAlive: Int) {
+
+    fun noOfChildrenList(): MutableList<Fish> {
+        var result = mutableListOf<Fish>()
+
+        val firstBirth = daysAlive - 9
+        for (i in firstBirth downTo 0 step 7) {
+            result.add(Fish(i))
+        }
+
+        return result
+    }
+
+    override fun toString(): String {
+        return "Fish(daysAlive=$daysAlive)"
+    }
+}
